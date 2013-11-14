9a488406ddde0dbcd90a5fda5cecf7faeaeec732
tjylazy <ty007008009@gmail.com>
2013-11-14 18:33:40 +0800

    框架

---
+++ application/views/admin_view.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+  <form class="form-horizontal">
+    <fieldset>
+      <div id="legend" class="">
+        <legend class="">±íµ¥Ãû</legend>
+      </div>
+    <div class="control-group">
+
+          <!-- Text input-->
+          <label class="control-label" for="input01">Text input</label>
+          <div class="controls">
+            <input type="text" placeholder="placeholder" class="input-xlarge">
+            <p class="help-block">Supporting help text</p>
+          </div>
+        </div>
+
+    <div class="control-group">
+
+          <!-- Text input-->
+          <label class="control-label" for="input01">Text input</label>
+          <div class="controls">
+            <input type="text" placeholder="placeholder" class="input-xlarge">
+            <p class="help-block">Supporting help text</p>
+          </div>
+        </div><div class="control-group">
+
+          <!-- Textarea -->
+          <label class="control-label">Textarea</label>
+          <div class="controls">
+            <div class="textarea">
+                  <textarea type="" class=""> </textarea>
+            </div>
+          </div>
+        </div><div class="control-group">
+
+          <!-- Select Basic -->
+          <label class="control-label">Select - Basic</label>
+          <div class="controls">
+            <select class="input-xlarge">
+              <option>Enter</option>
+              <option>Your</option>
+              <option>Options</option>
+              <option>Here!</option>
+            </select>
+          </div>
+
+        </div><div class="control-group">
+
+          <!-- Text input-->
+          <label class="control-label" for="input01">Text input</label>
+          <div class="controls">
+            <input type="text" placeholder="placeholder" class="input-xlarge">
+            <p class="help-block">Supporting help text</p>
+          </div>
+        </div><div class="control-group">
+          <label class="control-label">File Button</label>
+
+          <!-- File Upload -->
+          <div class="controls">
+            <input class="input-file" id="fileInput" type="file">
+          </div>
+        </div>
+
+    
+
+    <div class="control-group">
+          <label class="control-label">Button</label>
+
+          <!-- Button -->
+          <div class="controls">
+            <button class="btn btn-success">Button</button>
+          </div>
+        </div>
+
+    </fieldset>
+  </form>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
